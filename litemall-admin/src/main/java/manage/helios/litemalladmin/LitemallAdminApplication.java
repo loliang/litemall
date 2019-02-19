4dea34e8a46c71c9cf4bb9b8ed6192ca3ca2273d
loliang <wooleo@aliyun.com>
2019-02-19 14:47:42 +0800

    新增后台管理

---
+++ litemall-admin/src/main/java/manage/helios/litemalladmin/LitemallAdminApplication.java
@@ -0,0 +1,13 @@
+package manage.helios.litemalladmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitemallAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LitemallAdminApplication.class, args);
+	}
+
+}
